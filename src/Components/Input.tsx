c47fb2c5dad08c5616750c298270ff068cced2f3
Anna Wójcik <annawojcik@Annas-Mac-mini.local>
2018-05-22 11:41:57 +0200

    rejestracja update

--- src/Components/Input.tsx
+++ src/Components/Input.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 
 export namespace Types {
     export type InputProps = {
-        label: any;
+        label?: any;
         type: any;
         id: any;
         name: any;
